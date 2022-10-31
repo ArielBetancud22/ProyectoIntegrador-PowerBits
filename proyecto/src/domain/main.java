58eb74debd7696f6a76e4c56a7acad6a0516f28d
juaniM4c <juanimachuca1@gmail.com>
2022-10-31 17:49:28 -0300

    pruebaJuani

--- proyecto/src/domain/main.java
+++ proyecto/src/domain/main.java
@@ -6,6 +6,9 @@ public class main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
         
+        System.out.println("hoa");
+        
+        
         
         
         
