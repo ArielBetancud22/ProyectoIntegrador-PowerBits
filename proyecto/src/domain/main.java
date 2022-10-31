87056bf35b45a058aa99b1ae5ce5ada6c2bc1a77
IviiGonzalez <112676298+IviiGonzalez@users.noreply.github.com>
2022-10-31 17:47:34 -0300

    Nuevo cambio

--- proyecto/src/domain/main.java
+++ proyecto/src/domain/main.java
@@ -5,7 +5,7 @@ package domain;
 public class main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
-        
+        System.out.println("Como estan?");
         
         
         
