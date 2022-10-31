2e84680a80a467310557f397c7ce8d0a2c016e5d
Nico25Mac <112595518+Nico25Mac@users.noreply.github.com>
2022-10-31 17:49:58 -0300

    Update main.java

--- proyecto/src/domain/main.java
+++ proyecto/src/domain/main.java
@@ -6,7 +6,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
         System.out.println("Como estan?");
-        
+        System.out.println("El juani se come los mocos");
         
         
     }
