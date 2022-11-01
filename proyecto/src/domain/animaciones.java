96bf352f9bd31b9f69d4b27ee2ce9020a77181a2
Lucas Mendez <112028014+LucasMendez11@users.noreply.github.com>
2022-11-01 11:46:29 -0300

    1er avance
    agregamos el menu principal y el de jugadores

---
+++ proyecto/src/domain/animaciones.java
@@ -0,0 +1,34 @@
+
+package domain;
+
+
+public class animaciones {
+    
+    
+   
+    
+    public static void bienvenidos(){
+        System.out.println("           o o o       o o o     o o o     o     o    o       o    o o o    o     o    o o o     o o o         o o       o o o        ");
+        System.out.println("           o     o       o       o         o o   o     o     o     o        o o   o      o       o     o     o     o    o             ");
+        System.out.println("           o o o         o       o o o     o  o  o      o   o      o o o    o  o  o      o       o      o    o     o     o o o        ");
+        System.out.println("           o     o       o       o         o   o o       o o       o        o   o o      o       o     o     o     o          o       ");
+        System.out.println("           o o o       o o o     o o o     o     o        o        o o o    o     o    o o o     o o o         o o       o o o        ");
+    }
+    
+    public static void desarrolladores(){
+        System.out.println("                                         ----------------------------------------------");
+        System.out.println("                                                  DESARROLLADORES DEL PROYECTO         ");
+        System.out.println("                                         ----------------------------------------------");
+        System.out.println("                                         - Antonella Arraez");
+        System.out.println("                                         - Ivana Gonzalez");
+        System.out.println("                                         - Facundo Rios");
+        System.out.println("                                         - Héctor Fonzalida");
+        System.out.println("                                         - Juan Machuca");
+        System.out.println("                                         - Lucas Mendez");
+        System.out.println("                                         - Nadia Gonzalez");
+        System.out.println("                                         - Nicolás Machuca\n");
+        System.out.println("                                                     - P O W E R   B I T S -");
+    }
+    
+   
+}
--- proyecto/src/domain/main.java
+++ proyecto/src/domain/main.java
@@ -1,25 +1,96 @@
 
 package domain;
 
+import static domain.animaciones.bienvenidos;
+import java.util.Scanner;
+
 
 public class main {
+    
     public static void main(String[] args) {
-        System.out.println("hola mundo");
-        System.out.println("Como estan?");
-        System.out.println("El juani se come los mocos");
-        
-        System.out.println("askljdaklsd");
-        System.out.println("Juani gato");
-        
-        System.out.println("ksdaj vvvvvvvvv");
-        
+       bienvenidos();
+       menu();
         
     }
     
+    public static void menu(){
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        int opcion;
+        
+        do{
+            System.out.println("");
+            System.out.println("");
+            System.out.println("                                                      - - - M E N U   D E   O P C I O N E S - - -");
+            System.out.println("");
+            System.out.println("                                        1- Jugar Loteria");
+            System.out.println("                                        2- Jugar Quini6");
+            System.out.println("                                        3- Salir");
+            System.out.println("");
+            System.out.print("                                        - Ingrese una opcion: ");
+        opcion = Integer.parseInt(entrada.nextLine());
+        }while(opcion > 3 || opcion < 1);
+
+        
+        System.out.println("");
+        System.out.println("");
+        
+        
+        switch(opcion){
+            case 1:
+                menuJugadores();
+                
+        }
+
+    }
     
     
-    
-    
-    
+    public static void menuJugadores(){
+        
+        Scanner entrada = new Scanner(System.in); 
+        
+        int eleccion;
+        String nombre1, nombre2;
+        
+        
+        System.out.println("                                                                    L O T E R I A");
+        System.out.println("");
+        System.out.println("                                        1- Ingrese el nombre de los jugadores");
+        System.out.println("                                        2- Seleccion de los cartones");
+        System.out.println("                                        3- Jugar");
+        System.out.println("                                        4- Volver al menu principal");
+        System.out.println("");
+        System.out.print("                                        -Ingrese una opcion: ");
+        eleccion = Integer.parseInt(entrada.nextLine());
+        
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        
+        
+        
+        switch(eleccion){
+        
+            case 1:
+                System.out.println("                                                      ------------------------------------------");
+                System.out.print("                                                      Ingrese el nombre del JUGADOR 1: ");
+                nombre1 = entrada.nextLine();
+                System.out.println("                                                      ------------------------------------------");
+                System.out.println("");
+                System.out.println("                                                      ------------------------------------------");
+                System.out.print("                                                      Ingrese el nombre del JUGADOR 2: ");
+                nombre2 = entrada.nextLine();
+                System.out.println("                                                      ------------------------------------------");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+                
+        }
+        
+    }
     
 }
+
+
+
