63886ab9b4b2497e7c14aec78abbb4d40d111d4a
IviiGonzalez <112676298+IviiGonzalez@users.noreply.github.com>
2022-12-08 11:36:50 -0300

    Documentación del proyecto

--- proyecto/src/domain/main.java
+++ proyecto/src/domain/main.java
@@ -1,6 +1,6 @@
 
 package domain;
-
+//Importamos la animaciones y clase scanner 
 import static domain.animaciones.bienvenidos;
 import static domain.animaciones.cartonLleno;
 import static domain.animaciones.desarrolladores;
@@ -9,30 +9,29 @@ import static domain.animaciones.teclaSeguir;
 import java.util.Scanner;
 
 public class main {
-    
-    private int variante1;
+    //Atributos
+    private int variante1;         
     private int variante2;
     private int cont = 0;
-    private int nros[] = new int[100];
+    private int nros[] = new int[100];    //Array para numeros sorteados
 
     
     public static void main(String[] args) {
        bienvenidos();
        animaciones.teclaSeguir();
-       main me = new main();
+       main me = new main();       //Se llama a la  función menu de esta forma porque tienen diferente modificador de acceso
        me.menu(); 
     }
     
-    public void menu(){
-        
+    public void menu(){  
         Scanner entrada = new Scanner(System.in);
-        
+        //Variable que guardara la opcion de meu elegida por el usuario 
         int opcion;
-        
-        String nombre1 = " ";
+        // Se declaran las variables para los nombre de los jugadores
+        String nombre1 = " ";    
         String nombre2 = " ";
         
-        
+        //Se utiliza un do while para el menu de opciones 
         do{
             System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------------");
             System.out.println("");
@@ -51,7 +50,7 @@ public class main {
         System.out.println("");
         System.out.println("");
           
-        
+        //Switch que mostara la opcion elegia por el usuario 
         switch(opcion){
             case 1:
                 menuJugadores(nombre1, nombre2);
@@ -92,7 +91,7 @@ public class main {
         
         System.out.println("");
         
-        switch(eleccion){
+        switch(eleccion){   //Mediante un switch se muestra la distintas opciones del menu 
         
             case 1:
                 System.out.println("                                                      ------------------------------------------");
@@ -108,16 +107,17 @@ public class main {
                 System.out.println("");
                 animaciones.teclaSeguir();
                 System.out.println("");
-                menuJugadores(nombre1, nombre2);
+                menuJugadores(nombre1, nombre2);   // Regreso al menu de jugadores con los nombres ya guardados
+                animaciones.teclaSeguir();
             case 2:
-                seleccionCartones(nombre1, nombre2);
-                menuJugadores(nombre1, nombre2);                
+                seleccionCartones(nombre1, nombre2);  //Selecion de los cartones 
+                menuJugadores(nombre1, nombre2);      // Regreso al menu de jugadores con los cartones seleccionados          
             case 3:
-                sortear(nombre1, nombre2);
+                sortear(nombre1, nombre2);   // Llamamos al método sortear para jugar
                 animaciones.teclaSeguir();
                 menu();
             case 4:
-                menu();
+                menu();   //regreso al menu principal
         }    
     }
     
@@ -167,8 +167,9 @@ public class main {
             seleccionCartones(nombre1, nombre2);
         }
         System.out.println("                              ------------------------------------------------------------------------------------");
-
-        if (cartonElegido == 1) { 
+        
+        // if que mostara las opciones de cartones para el jugador 2 despues de haya elegido el jugador 1 
+        if (cartonElegido == 1) { //Si el carton elegido es igual a 1 se mostrara solo los cartones 2, 3 y 4
             carton_2();
             carton_3();
             carton_4();
@@ -237,20 +238,22 @@ public class main {
         String carton1[][] = new String[3][9];
         String carton2[][] = new String[3][9];
         
+        // Se arman los cartones segun la variante seleccionada
         armadoCartonPorVariante(variante1, carton1);
         armadoCartonPorVariante(variante2, carton2);
         
+        // Bucle para recorrer los cartones hasta que haya un carton lleno
         do{
             System.out.println("");
             System.out.println("");
             animaciones.teclaSeguir();
-            nro_sorteado = obtenerSinRepetir();
+            nro_sorteado = obtenerSinRepetir(); // Obtenemos un numero que aun no haya salido
             
             for(int i=0; i<=2; i++){         //Mediante el siguiente for se realiza la comprobación de coincidencias dentro de cada carton y 
                 for(int j=0; j<=8; j++){    //a su misma vez reemplazando los numeros sorteados con un "X"
-                    if(Integer.toString(nro_sorteado).equals(carton1[i][j].replaceAll("\\s", ""))){
-                        carton1[i][j] = " X";
-                        cont_carton1++;
+                    if(Integer.toString(nro_sorteado).equals(carton1[i][j].replaceAll("\\s", ""))){ // Usamos equals para comparar entre dos strings. replaceAll es para eliminar los espacios vacios que trae el carton
+                        carton1[i][j] = " X"; // Si existe el nro_sorteado en el carton lo cambiamos por una X
+                        cont_carton1++;// Se le suma 1 al contador del carton por cada coincidencia
                     }
                     if(Integer.toString(nro_sorteado).equals(carton2[i][j].replaceAll("\\s", ""))){
                         carton2[i][j] = " X";
@@ -261,7 +264,9 @@ public class main {
             System.out.println("");
             System.out.println("           [-> "+nombre1+" <-]");
             System.out.println("");
-            mostrarCartonPorVariante(variante1,carton1);
+            
+            mostrarCartonPorVariante(variante1,carton1); // MOstramos el carton actualizado con las coincidencias
+            
             System.out.println("           [-> "+nombre2+" <-]");
             mostrarCartonPorVariante(variante2,carton2);
             System.out.println("");
@@ -270,19 +275,19 @@ public class main {
             System.out.println("");
             
             System.out.println("\n                                              -> LISTA DE NUMEROS YA SORTEADOS <-");
-            for(int i=0; i<cont; i++){
+            for(int i=0; i<cont; i++){ // Ek for es para mostrar los numeros ya sorteados
                 if(this.nros[i] > 0){
                     System.out.print(" " + this.nros[i]);
                 }
-                if (i == 45) {
+                if (i == 45) { // cuando el contador sea igual a 45 reliza un salto de linea
                     System.out.println("");
                 }
             }
+            
             System.out.println("");
 
-            if(cont_carton1 == 15){
-                System.out.flush();
-                
+            // Si el contador de cada carton llega a 15 (es la cantidad de numeros por carton) es CartonLLeno
+            if(cont_carton1 == 15){                
                 animaciones.cartonLleno();
                 System.out.println("");
                 System.out.println("                                           FELICITACIONES "+nombre1+" HAS GANADO EL JUEGO");
@@ -290,7 +295,6 @@ public class main {
             }
             
             if(cont_carton2 == 15){
-                System.out.flush();
                 animaciones.cartonLleno();
                 System.out.println("");
                 System.out.println("                                           FELICITACIONES "+nombre2+" HAS GANADO EL JUEGO");
@@ -305,31 +309,26 @@ public class main {
         int nro_sorteado = 0;
         boolean repetido;
         
-        for (int i = 1; i <= 1; i++) {
-//            System.out.println("Contador: " + this.cont);
+        for (int i = 1; i <= 1; i++) { // Inicio un bucle de una sola vuelta para obtener 1 unico nro
             repetido = false;
-            nro_sorteado = (int)(Math.random()*((90-0)+1));
+            nro_sorteado = (int)(Math.random()*((90-0)+1)); // Obtengo un numero aleatorio hasta 90
             
-//            System.out.println("NRO SORTEADO: " + nro_sorteado);
-            
-//            System.out.println("\nCOMPARA: ");
-            for(int j=1; j <= this.cont; j++) {
-//                System.out.print(" " + this.nros[j]+ " ");
-                if (nro_sorteado == this.nros[j]) {
+            for(int j=1; j <= this.cont; j++) { // Busco el nuevo numero en los nros ya sorteados 
+                if (nro_sorteado == this.nros[j]) { // Si ya existe seteo repetido=true
                     repetido=true; 
                 }                
             }
-            if (repetido == true) {
+            
+            if (repetido == true) {//Si repetido=true le resto 1 al contador para que vuelva a iniciarse
                 i--;
-            } else {
+            } else { // Sino lo cargo a los numeros y aumento 1 al contador
                 this.nros[this.cont] = nro_sorteado;
                 this.cont++;
-                this.nros[this.cont] = 0;
             }
         }
         return nro_sorteado;
     }
-    
+    // Se arma el carton segun la variante(variante= opcion de carton elegida)
     public static void armadoCartonPorVariante(int variante, String[][] carton){
         if (variante == 1){
             armadoCarton1(carton);
@@ -347,7 +346,7 @@ public class main {
             }
         }
     }
-    
+    //Se muestra el carton segun la variante 
     public static void mostrarCartonPorVariante(int variante, String[][] carton){
         if (variante == 1){
             mostrarCarton1(carton);
@@ -365,26 +364,29 @@ public class main {
             }
         }
     }
-
+    //// Armado del carton, Cargamos los numeros del carton en una matris y colocamos ceros en los espacios que luego iran en blanco
     public static void armadoCarton1(String[][] carton1){
         
         carton1[0][0] = " 1"; carton1[0][1] = "0"; carton1[0][2] = "0"; carton1[0][3] = "31"; carton1[0][4] = "0"; carton1[0][5] = "57"; carton1[0][6] = "65"; carton1[0][7] = "0"; carton1[0][8] = "88"; 
         carton1[1][0] = "0"; carton1[1][1] = "12"; carton1[1][2] = "0"; carton1[1][3] = "35"; carton1[1][4] = "47"; carton1[1][5] = "0"; carton1[1][6] = "0"; carton1[1][7] = "75"; carton1[1][8] = "0";
         carton1[2][0] = " 8"; carton1[2][1] = "19"; carton1[2][2] = "0"; carton1[2][3] = "0"; carton1[2][4] = "48"; carton1[2][5] = "0"; carton1[2][6] = "67"; carton1[2][7] = "76"; carton1[2][8] = "90"; 
     }
+    //Mostramos el carton
     public static void mostrarCarton1(String[][] carton1) {
         System.out.println("");
         System.out.println("                                        CARTON N°1");
         System.out.println("");
-        System.out.println("                                        ----------------------------------------------");
+        System.out.println("                                        ----------------------------------------------");  // Linea que delimitara al carton en la parte superior
         
-        for (int i = 0; i <= 2; i++) {
+        for (int i = 0; i <= 2; i++) {   //For para recorrer el carton 
             System.out.print("                                        |");
             for (int j = 0; j <= 8; j++) {
-                if (carton1[i][j] == "0"){
+                if (carton1[i][j] == "0"){   // Se reemplaza con un espacio en blanco  cada cero que se encuetre en el carton
                     carton1[i][j] = "  "; 
                 }
-                if (carton1.length > 1){
+                // Alineacion de los numeros del carton a traves de un if
+                if (carton1.length > 1){  // Si la longuitud del caracter es mayor a 1 dejo un espacio delante y otro atras del mismo, 
+                                          // si no dejo un esacio adelante y doble espacio por detras 
                     System.out.print(" "+carton1[i][j]+ " |");
                 }else{
                     System.out.print(" "+carton1[i][j]+ "  |");
@@ -392,7 +394,7 @@ public class main {
             } 
             System.out.println("");
         }
-        System.out.println("                                        ----------------------------------------------");
+        System.out.println("                                        ----------------------------------------------");  // Linea que delimitara al carton en la parte inferior
      }
     
     public static void armadoCarton2(String[][] carton2){
@@ -484,10 +486,10 @@ public class main {
         }
         System.out.println("                                        ----------------------------------------------");
     }
-    
-    public static void carton_1(){
+    // Metodo para mostar los cartones 
+    public static void carton_1(){  
         String carton1[][] = new String[3][9]; 
-        armadoCarton1(carton1); 
+        armadoCarton1(carton1);  //Llamamos al metodo de armado y mostar carton  
         mostrarCarton1(carton1);
     }
     public static void carton_2(){
